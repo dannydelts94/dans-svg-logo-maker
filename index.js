86d0b2b9fc67b584aad47b6125a9e3e62e2347d6
dannydelts94 <126992132+dannydelts94@users.noreply.github.com>
2023-04-17 20:18:29 -0400

    shape generation

--- index.js
+++ index.js
@@ -3,8 +3,24 @@ const fs = require('fs');
 const { Circle, Triangle, Square } = require('./lib/shapes');
 
 function writeToFile(fileName, answers) {
-let svgString = 
+let svgShapeString = '';
+svgShapeString = 
+'<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200"></svg>';
+svgShapeString += '<g>';
+svgShapeString += `${answers.shapeList}`;
+let shapeSelection;
+if (answers.shapeList === 'Circle') {
+    shapeSelection = new Circle();
+    svgShapeString += `<circle cx="150" cy="100" r="80" fill="${answers.shapeColor}"/>`;
+} else if (answers.shapeList === 'Triangle') {
+    shapeSelection = new Triangle();
+    svgShapeString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeColor}"/>`;
+} else {
+    shapeSelection = new Square();
+    svgShapeString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeColor}"/>`;
 }
+}
+
 
 const parameters = [
     {
